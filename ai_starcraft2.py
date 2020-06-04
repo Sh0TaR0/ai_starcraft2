36709c5bb642f7348defbf3bda849bfa4cf8d7e6
小林翔太郎 <sho@MBP.local>
2020-06-04 09:17:27 +0900

    ep3

--- ai_starcraft2.py
+++ ai_starcraft2.py
@@ -1,7 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT
+from sc2.constants import COMMANDCENTER, SCV, SUPPLYDEPOT, REFINERY, BARRACKS, MARINE
 
 
 class PythonAI(sc2.BotAI):
@@ -9,6 +9,9 @@ class PythonAI(sc2.BotAI):
         await self.distribute_workers()
         await self.build_workers()
         await self.build_supplydepots()
+        await self.build_refinerys()
+        await self.build_barracks()
+        await self.build_marines()
 
     async def build_workers(self):
         for cc in self.units(COMMANDCENTER).ready.noqueue:
@@ -21,6 +24,26 @@ class PythonAI(sc2.BotAI):
             if ccs.exists:
                 if self.can_afford(SUPPLYDEPOT):
                     await self.build(SUPPLYDEPOT, near=ccs.first)
+
+    async def build_refinerys(self):
+        for cc in self.units(COMMANDCENTER).ready:
+            gases = self.state.vespene_geyser.closer_than(10.0, cc)
+            for gas in gases:
+                if self.supply_used > 14 and self.can_afford(REFINERY):
+                    if self.units(REFINERY).amount < 2:
+                        worker = self.select_build_worker(gas.position)
+                        await self.do(worker.build(REFINERY, gas))
+
+    async def build_barracks(self):
+        if self.units(SUPPLYDEPOT).ready.exists:
+            sd = self.units(SUPPLYDEPOT).ready.random
+            if self.can_afford(BARRACKS):
+                await self.build(BARRACKS, near=sd)
+
+    async def build_marines(self):
+        for racks in self.units(BARRACKS).ready.noqueue:
+            if self.can_afford(MARINE):
+                await self.do(racks.train(MARINE))
 
 
 run_game(maps.get("AbyssalReefLE"),
